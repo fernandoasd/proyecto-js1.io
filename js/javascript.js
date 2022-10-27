48406ffe43c4cc11d187b399d5bf96d810f332dd
fernandoasd <113261356+fernandoasd@users.noreply.github.com>
2022-10-26 22:53:37 -0300

    First commit

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+var selected_js = document.getElementById("seleccion"); 
+var total_pagar_js = document.getElementById("total_pagar"); 
+var resumen_js = document.getElementById("btn_resumen"); 
+var cantidad_js = document.getElementById("cantidad");
+var borrar_js = document.getElementById("btn_borrar");
+var nombre_js = document.getElementById("nombre");
+var apellido_js = document.getElementById("apellido");
+var mail_js = document.getElementById("mail");
+
+
+
+
+document.getElementById('btn_resumen').addEventListener('click', resumen);
+document.getElementById('btn_resumen').addEventListener('click', ver);
+document.getElementById('btn_borrar').addEventListener('click', borrar);
+
+function resumen(){
+    switch (selected_js.value){
+        case selected_js.value="Estudiante":
+            console.log("seleccion= 1->" + selected_js.value + " TOTAL : " + total_pagar_js.value);
+            total_pagar_js.value= "Total a Pagar: $" + 200*0.2*cantidad_js.value;
+        break;
+        case selected_js.value="1":
+            console.log("seleccion= 1->" + selected_js.value);
+            total_pagar_js.value = "Total a Pagar: $" + 200*0.5*cantidad_js.value;
+        break;
+        case selected_js.value="2":
+            console.log("seleccion= 2->" + selected_js.value);
+            total_pagar_js.value = "Total a Pagar: $" + 200*0.85*cantidad_js.value;
+        break;
+    }
+}
+
+function ver (){
+    console.log("seleccion= " + selected_js.value + " Total : " + total_pagar_js.value);
+}
+
+function borrar() {
+    nombre_js.value= "";
+    apellido_js.value= "";
+    mail_js.value= "";
+    cantidad_js.value= "";
+    total_pagar_js.value = "Total a Pagar: $";
+}
